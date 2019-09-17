d9714a30c7ba1b2b4204722f109fa461bbca487a
Kara Odle <karajo.odle@gmail.coml>
2019-09-17 12:38:07 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @user_name = params[:name]
+    "#{@user_name.reverse}"
+  end
+
+  get '/square/:number' do
+    erb :square
+  end
+
+  get '/say/:number/:phrase' do
+    erb :number_phrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    erb :sentence
+  end
+
+  get '/:operation/:number1/:number2' do
+    erb :operation
+  end
 
 end
